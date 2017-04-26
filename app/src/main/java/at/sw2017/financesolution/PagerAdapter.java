a2e3fa4aef82cee4cc188b11fd4e2673604dab8e
Josef Koini <post@koini.at>
2017-04-26 16:23:15 +0200

    [JK,HF] tests and part of the implementation for main activity tabs + fragments

---
+++ app/src/main/java/at/sw2017/financesolution/PagerAdapter.java
@@ -0,0 +1,10 @@
+package at.sw2017.financesolution;
+
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by joe on 26.04.17.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+}
