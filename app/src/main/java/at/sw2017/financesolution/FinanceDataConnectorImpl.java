5c97b55e37e1a2ec448f3a01515b783dea4027db
Fabio Wallner <fabio.wall@gmail.com>
2017-05-09 15:00:13 +0200

    [FW, JK] renamed variables

--- app/src/main/java/at/sw2017/financesolution/FinanceDataConnectorImpl.java
+++ app/src/main/java/at/sw2017/financesolution/FinanceDataConnectorImpl.java
@@ -221,14 +221,14 @@ public class FinanceDataConnectorImpl extends SQLiteOpenHelper implements Financ
         String args[] = {};
         Cursor cursor = this.getReadableDatabase().rawQuery(queryAllCategories, args);
 
-        ArrayList<Category> al = new ArrayList<Category>();
+        ArrayList<Category> categoriesList = new ArrayList<Category>();
         cursor.moveToNext();
         while(!cursor.isAfterLast()) {
-            Category cat = new Category();
-            cat.setName(cursor.getString(cursor.getColumnIndex(KEY_NAME)));
-            cat.setId(cursor.getLong(cursor.getColumnIndex(KEY_ID)));
+            Category newCategory = new Category();
+            newCategory.setName(cursor.getString(cursor.getColumnIndex(KEY_NAME)));
+            newCategory.setId(cursor.getLong(cursor.getColumnIndex(KEY_ID)));
 
-            al.add(cat);
+            categoriesList.add(newCategory);
             cursor.moveToNext();
         }
 
