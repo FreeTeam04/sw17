eb96340f7bb6e3aef6d4b5b75b70f5e5dfe15c06
Peter Egger <peter.egger@student.tugraz.at>
2017-05-09 21:24:19 +0200

    [PE,HF] added tests for addTransactionActivity

--- app/src/androidTest/java/at/sw2017/financesolution/AddTransactionActivityInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017/financesolution/AddTransactionActivityInstrumentedTest.java
@@ -1,19 +1,21 @@
 package at.sw2017.financesolution;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
-import static android.support.test.espresso.Espresso.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by fabiowallner on 09/05/2017.
@@ -25,6 +27,64 @@ public class AddTransactionActivityInstrumentedTest {
     public ActivityTestRule<AddTransactionActivity> mActivityRule = new
             ActivityTestRule<>(AddTransactionActivity.class);
 
+
+    // Test if UI elements are displayed
+    @Test
+    public void testAddTransactionHeaderExists() throws Exception {
+        onView(withId(R.id.add_transaction_header)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testDescriptionTextFieldExists() throws Exception {
+        onView(withId(R.id.editDescription)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSpinnerGategoryExists() throws Exception {
+        onView(withId(R.id.spinnerGategory)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSignButtonExists() throws Exception {
+        onView(withId(R.id.sign)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSignButtonExists() throws Exception {
+        onView(withId(R.id.sign)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEditAmountExists() throws Exception {
+        onView(withId(R.id.editAmount)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testDatePickerExists() throws Exception {
+        onView(withId(R.id.datepickerDate)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSaveButtonExists() throws Exception {
+        onView(withId(R.id.buttonSave)).check(matches(isDisplayed()));
+    }
+
+
+    // Test Input
+    @Test
+    public void testDescriptionInput() throws Exception {
+
+        final String TEXT = "Test Description Text!";
+
+        // write description text
+        onView(withId(R.id.editDescription))
+                .perform(typeText(TEXT), closeSoftKeyboard());
+
+        // check description text
+        onView(withId(R.id.editDescription))
+                .check(matches(withText(TEXT)));
+    }
+    
 
     @Test
     public void testSaveButton() throws Exception {
